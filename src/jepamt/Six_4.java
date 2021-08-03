37f6adf50ff656c64af7ecf042ca1fe3eed73317
gg.gg <gg.gg@gg.gg>
2021-08-03 14:01:39 +0300

    non-blocking

--- src/jepamt/Six_4.java
+++ src/jepamt/Six_4.java
@@ -2,10 +2,12 @@ package jepamt;
 
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,6 +22,11 @@ public class Six_4 {
 class Boat {
     private final int boatId;
     private final int maxLoad;
+    @Override
+    public String toString() {
+	return "Boat [boatId=" + boatId + ", maxLoad=" + maxLoad + ", currentLoad=" + currentLoad + "]";
+    }
+
     private int currentLoad;
     private boolean state;
 
@@ -72,7 +79,7 @@ class Port {
     private AtomicInteger currentLoad = new AtomicInteger(5000);
     private final int maxContainerCapacity = 5000;
     private List<Dock> docks = new CopyOnWriteArrayList<Dock>();
-    public Queue<Boat> boats = new ConcurrentLinkedQueue<Boat>();
+    private Queue<Boat> boats = new LinkedBlockingQueue<Boat>();
     private ExecutorService exec;
 
     void init() {
@@ -101,12 +108,16 @@ class Port {
 		}
 		int max = tlr.nextInt(1, 1001);
 		boolean b = false;// = tlr.nextBoolean();
-		if (tlr.nextInt(100) < 30) {
+		if (tlr.nextInt(100) < 40) {
 		    b = true;
 		}
 		if (!stop) {
 		    Boat boat = new Boat(i++, tlr.nextInt(0, max), max, b);
 		    boats.add(boat);
+		    System.err.println("New boat enter in port:" +boat);
+		    synchronized (p) {
+			p.notifyAll();
+		    }
 		}
 		try {
 		    Thread.currentThread().sleep(500);
@@ -174,7 +185,7 @@ class Dock implements Runnable {
     @Override
     public void run() {
 	while (!Thread.currentThread().isInterrupted()) {
-	    Boat boat;	    
+	  /*  Boat boat;	    
 	    while ((boat = port.getBoat()) == null) {
 		try {
 		    System.out.printf("dock %d sleep...\n", dockId);
@@ -218,7 +229,58 @@ class Dock implements Runnable {
 		    "boatID: %d, portLoad: %d, startBoatLoad: %d, currentBoatState: %s, boatMaxLoad %d, boatCurrentLoad %d\n",
 		    boat.getBoatId(), port.getCurrentLoad(), startLoad, boat.getState(), boat.getMaxLoad(),
 		    boat.getCurrentLoad());
+	    port.addBoat(boat);*/
+	    Boat boat;	    
+	    while ((boat = port.getBoat()) == null) {
+		try {
+		    System.out.printf("dock %d wait...\n", dockId);
+		    synchronized (port) {
+			port.wait();
+		    }
+		} catch (InterruptedException e) {
+		    System.out.println(Thread.currentThread().getName() + " interrupted.");
+		    e.printStackTrace();
+		}
+	    }
+	    int startPortLoad=port.getCurrentLoad();
+	    int startLoad = boat.getCurrentLoad();
+	    if (boat.getState()) {
+		while (boat.getCurrentLoad() > 0) {
+		    if (port.addContainer()) {
+			boat.removeContainer();
+		    }
+		    if (port.getCurrentLoad() == port.getMaxContainerCapacity()) {
+			break;
+		    }
+		}
+	    } else {
+		while (boat.getCurrentLoad() < boat.getMaxLoad()) {
+		    if (port.removeContainer()) {
+			boat.addContainer();
+		    }
+		    if (port.getCurrentLoad() <1) {
+			break;
+		    }
+		}
+	    }
+	    try {
+		TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(200, 1001));
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+	    if (boat.getCurrentLoad() == 0) {
+		boat.setState(false);
+	    } else {
+		boat.setState(true);
+	    }
+	    String state=boat.getState()==true?"loading ON boat":"unloading FROM boat";
+	    System.out.printf("Dock id: %d, boatID: %d, startPortLoad: %d,  startBoatLoad: %d, currentBoatState: %s, boatMaxLoad %d, boatCurrentLoad %d, afterWorkPortLoad: %d\n",
+		    dockId, boat.getBoatId(), port.getCurrentLoad(), startLoad, state, boat.getMaxLoad(),
+		    boat.getCurrentLoad(), startPortLoad);
 	    port.addBoat(boat);
+	    synchronized (port) {
+		port.notifyAll();
+	    }
 	}
     }
 }
