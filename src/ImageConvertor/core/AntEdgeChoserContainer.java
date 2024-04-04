dc3fb7c71dc03d1110a97da26c7fb807c2557a70
Roman <Roman@WinX.WinX>
2024-04-04 23:40:14 +0300

    ddda

---
+++ src/ImageConvertor/core/AntEdgeChoserContainer.java
@@ -0,0 +1,11 @@
+package ImageConvertor.core;
+
+import ImageConvertor.core.AntPathWorker.DataHolder;
+
+public class AntEdgeChoserContainer {
+
+	public float chanceToGoHere;
+	public AntEdge antEdge;
+	public DataHolder dataHolderEdge;
+
+}
